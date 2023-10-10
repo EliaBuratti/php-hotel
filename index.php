529c4c10a92ab1d256aeeccfb6aeb03703f15f0f
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-10 15:20:53 +0200

    create data dinamically on table

--- index.php
+++ index.php
@@ -113,29 +113,45 @@ $hotels = [
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+
+                <?php $i = 1;
+                foreach ($hotels as $key => $value) : ?>
+                    <tr>
+                        <th scope="row"><?= $i++ ?></th>
+
+                        <td><?= $value['name'] ?></td>
+
+                        <td><?= $value['description'] ?></td>
+
+                        <td>
+
+                            <?php if ($value['parking']) : ?>
+
+                                Yes
+
+                            <?php else : ?>
+
+                                No
+
+                            <?php endif; ?>
+
+                        </td>
+
+
+                        <td><?= $value['vote'] ?></td>
+
+                        <td><?= $value['distance_to_center'] . ' km' ?></td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
